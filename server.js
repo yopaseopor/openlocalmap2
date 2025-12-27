694cffba89f82a9c76987a4cbf39ca24d80920a1
yopaseopor <yopaseopor@gmail.com>
2025-12-28 00:45:01 +0100

    Fix Pages deploy: publish to gh-pages; add base href and OG URLs; add CORS handling

--- server.js
+++ server.js
@@ -5,8 +5,12 @@ const app = express();
 // Enable CORS for all routes
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
+  // Respond to preflight requests
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204);
+  }
   next();
 });
 
